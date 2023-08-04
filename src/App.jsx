7b21cad6e1b9cfe2ab31ac7364800e58b68f4f5a
NicolasCirulli <nicolascirulli2@gmail.com>
2023-08-03 21:21:52 -0300

    workshop state y useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home/Home";
+import Events from "./pages/Events/Events";
+
 function App() {
   return (
     <>
       <Home />
-      {/*   <Events /> */}
+      {/*  <Events /> */}
     </>
   );
 }
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,5 +1,57 @@
+import "./carousel.css";
+import { useState, useEffect } from "react";
+
 const Carousel = () => {
-  return <h2>Carousel</h2>;
+  const [contador, setContador] = useState(0);
+  const [datos, setDatos] = useState([]);
+  const prev = () => {
+    if (contador == 0) {
+      setContador(events.length - 1);
+    } else {
+      setContador(contador - 1);
+    }
+  };
+
+  useEffect(() => {
+    // este como tiene el array vacio, se ejecuta solo cuando monta el componente
+    fetch("https://mindhub-xj03.onrender.com/api/amazing")
+      .then((response) => response.json())
+      .then((data) => setDatos(data.events));
+  }, []);
+
+  useEffect(() => {
+    // este useEffect se ejecuta cada vez que cambia el estado contador
+    console.log("Cuando cambia contador:", contador);
+  }, [contador]);
+
+  const next = () => {
+    if (contador == datos.length - 1) {
+      setContador(0);
+    } else {
+      setContador(contador + 1);
+    }
+  };
+
+  return (
+    <div className="carousel">
+      <button onClick={prev}> {"<"} </button>
+
+      {datos.length > 0 ? (
+        <img className="carousel-img" src={datos[contador].image} alt="" />
+      ) : (
+        <h2> Cargando... </h2>
+      )}
+
+      <button onClick={next}> {">"} </button>
+    </div>
+  );
 };
 
 export default Carousel;
+
+// renderiza monta re-render desmonta
+
+// useEffect -> sin array en todos los re-render
+// useEffect -> [] solo cuando monta
+// useEffect -> [ estado ] cuando cambia ese estado
+// useEffect -> [ estado1, estado2 ] cuando cambia cualquiera de esos estados
